f8ffc66d5886a371e41fde2cf532ccbb0e7a65be
Tim Kuehn <tikue@google.com>
2019-09-22 17:01:13 -0500

    Add an event loop to main.
    Currently 5 ticks/s.

--- src/bin/fakeblok.rs
+++ src/bin/fakeblok.rs
@@ -1,22 +1,30 @@
-use fakeblok::game::*;
+use fakeblok::game::{Game, Point};
 use log::info;
 use piston_window::{
-    clear, Button, ButtonArgs, ButtonState, Event, EventLoop, Input, Key, OpenGL, PistonWindow,
-    WindowSettings,
+    clear, Button, ButtonArgs, ButtonState, Event, EventLoop, EventSettings, Events, Input, Loop,
+    OpenGL, PistonWindow, WindowSettings,
 };
 use pretty_env_logger;
 
-fn get_key(event: &Event) -> Option<&Key> {
-    match event {
-        Event::Input(
-            Input::Button(ButtonArgs {
-                button: Button::Keyboard(ref key),
-                state: ButtonState::Press,
-                ..
-            }),
-            _,
-        ) => Some(key),
-        _ => None,
+fn process_input(game: &mut Game, input: &Input) {
+    if let Input::Button(ButtonArgs {
+        button: Button::Keyboard(key),
+        state: ButtonState::Press,
+        ..
+    }) = input
+    {
+        game.process_key(key);
+    }
+}
+
+fn process_loop(game: &mut Game, lp: &Loop) {
+    match lp {
+        Loop::Idle(_) => {}
+        Loop::Update(_) => {
+            game.move_entity_up(2);
+        }
+        Loop::AfterRender(_) => {}
+        lp => panic!("Didn't expect {:?}", lp),
     }
 }
 
@@ -30,16 +38,21 @@ fn main() {
         .unwrap();
     window.set_lazy(true);
 
+    let mut events = Events::new(EventSettings::new().ups(5));
     info!("start!");
     let mut game = Game::new(Point { x: 200, y: 200 }, 50);
-    while let Some(e) = window.next() {
-        if let Some(key) = get_key(&e) {
-            game.process_key(key);
+    while let Some(event) = events.next(&mut window) {
+        match event {
+            Event::Input(ref input, _) => process_input(&mut game, input),
+            Event::Loop(Loop::Render(_)) => {
+                window.draw_2d(&event, |c, g, _| {
+                    clear([1.0; 4], g);
+                    game.draw(c, g);
+                });
+            }
+            Event::Loop(ref lp) => process_loop(&mut game, lp),
+            _ => {}
         }
-        window.draw_2d(&e, |c, g, _| {
-            clear([1.0; 4], g);
-            game.draw(c, g);
-        });
     }
     info!("end :(");
 }
